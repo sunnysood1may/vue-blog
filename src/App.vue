ef3cfd15758c75ebaadeae6da8b3b838a51b6506
sunny sood <sunny1may@gmail.com>
2024-12-09 17:59:57 +0530

    interpolation

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your VueJS3 App"/>
+  <HomeComp />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import HomeComp from './components/Home.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HomeComp
   }
 }
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Home component 
+        {{10*20}}
+        {{"hello".length}}
+        {{email}}
+        {{getName('sood')}}
+        </h1>
+</template>
+
+<script>
+export default {
+    name:'HomeComp',
+    data(){
+        return{
+            email: 'sunny1may@gmail.com',
+            getName: function(e){
+                return e;
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: orange;
+}
+</style>
