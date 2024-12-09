976ee694ee58e9817cdfa8373b8819381a6ebfa5
sunny sood <sunny1may@gmail.com>
2024-12-09 22:29:00 +0530

    twoWayBinding

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,23 +3,32 @@
   <button v-on:click="getData('button clicked')">Click me</button><br /><br />
   <button v-on:dblclick="getData('button double clicked')">Click me</button>
 
-  <h2>{{count}}</h2>
+  <h3>Two way binding</h3>
+  <input type="text" v-model="count" />
+  <h2>{{ count }}</h2>
+
+  <h3>Get input field Value</h3>
+  <input type="text" placeholder="enter email" v-model="iemail" /> <br /><br />
+  <input type="password" placeholder="enter password" v-model="ipassword" /> <br /><br />
+  <button v-on:click="getData('function called')" type="button">Get Values</button>
 </template>
 
 <script>
 export default {
-    name: "HomeComp",
-    data() {
-        return {
-            count: 0,
-            email: "sunny1may@gmail.com",
-            /*getName: function (e) {
+  name: "HomeComp",
+  data() {
+    return {
+      iemail: null,
+      ipassword: null,
+      count: 0,
+      email: "sunny1may@gmail.com",
+      /*getName: function (e) {
                 return e;
             },*/
-        } 
-    },
-    methods:{
-        /*getName(e){
+    };
+  },
+  methods: {
+    /*getName(e){
             return e;
         },
         getData(){
@@ -28,14 +37,15 @@ export default {
                 email: this.email
             }
         },*/
-        getData(e){
-            alert(e);
-            this.count = this.count+1;
-        },
-        getConsole(){
-            console.log("function called");
-        }
-    }
+    getData(e) {
+      alert(e);
+      this.count = this.count + 1;
+      console.log(this.iemail,this.ipassword);
+    },
+    getConsole() {
+      console.log("function called");
+    },
+  },
 };
 </script>
 
