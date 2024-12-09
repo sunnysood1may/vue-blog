1c14a20dc16b7c7c52b194c46b9ef1bc5292563c
sunny sood <sunny1may@gmail.com>
2024-12-09 20:04:42 +0530

    method params

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,28 +1,37 @@
 <template>
-    <h1>Home component 
-        {{10*20}}
-        {{"hello".length}}
-        {{email}}
-        {{getName('sood')}}
-        </h1>
+  <h1>Home component</h1>
+  <h2>Name : {{ getName("sunny sood") }}</h2>
+  <h2>Email : {{ email }}</h2>
+  <h2>All Data : {{ getData().email }}</h2>
 </template>
 
 <script>
 export default {
-    name:'HomeComp',
-    data(){
-        return{
-            email: 'sunny1may@gmail.com',
-            getName: function(e){
+    name: "HomeComp",
+    data() {
+        return {
+            email: "sunny1may@gmail.com",
+            /*getName: function (e) {
                 return e;
+            },*/
+        } 
+    },
+    methods:{
+        getName(e){
+            return e;
+        },
+        getData(){
+            return {
+                name: "shilpa",
+                email: this.email
             }
         }
     }
-}
+};
 </script>
 
 <style scoped>
 h1 {
-    color: orange;
+  color: orange;
 }
 </style>
