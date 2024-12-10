f1ad38fe1ef007e4bad1e54e3a1bc991ab1b23fb
sunny sood <sunny1may@gmail.com>
2024-12-10 08:45:14 +0530

    getCheckRadio

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,8 +9,39 @@
 
   <h3>Get input field Value</h3>
   <input type="text" placeholder="enter email" v-model="iemail" /> <br /><br />
-  <input type="password" placeholder="enter password" v-model="ipassword" /> <br /><br />
-  <button v-on:click="getData('function called')" type="button">Get Values</button>
+  <input type="password" placeholder="enter password" v-model="ipassword" />
+  <br /><br />
+  <button v-on:click="getData('function called')" type="button">
+    Get Values
+  </button>
+
+  <h1>Get Checkbox & Radio Button Value</h1>
+  <h3>Technology</h3>
+  <label for="java">Java</label>
+  <input type="checkbox" value="java" v-model="technology" id="java" />
+  <label for="node">Node</label>
+  <input type="checkbox" value="node" v-model="technology" id="node" />
+  <label for="php">Php</label>
+  <input type="checkbox" value="php" v-model="technology" id="php" />
+  <h4>Technology : {{ technology }}</h4>
+  <h3>Profession</h3>
+  <label for="student">Student</label>
+  <input
+    type="radio"
+    value="student"
+    id="student"
+    v-model="profession"
+    name="profession"
+  />
+  <label for="developer">Developer</label>
+  <input
+    type="radio"
+    value="developer"
+    id="developer"
+    v-model="profession"
+    name="profession"
+  />
+  <h4>I am a {{ profession }}</h4>
 </template>
 
 <script>
@@ -18,6 +49,8 @@ export default {
   name: "HomeComp",
   data() {
     return {
+      technology: [],
+      profession: null,
       iemail: null,
       ipassword: null,
       count: 0,
@@ -40,7 +73,7 @@ export default {
     getData(e) {
       alert(e);
       this.count = this.count + 1;
-      console.log(this.iemail,this.ipassword);
+      console.log(this.iemail, this.ipassword);
     },
     getConsole() {
       console.log("function called");
